1534c837ca1e0ebd232270122e6604d6de543d4a
[diberr] <[diberrdigital@gmail.com]>
2021-09-10 12:26:55 +0100

    menu

--- script.js
+++ script.js
@@ -3,12 +3,27 @@ $(document).ready(function () {
     $('.overlay').fadeToggle(200);
     $(this).toggleClass('btn-open').toggleClass('btn-close');
   });
-  $('.overlay').on('click', function () {
+  // $('.overlay').on('click', function () {
+  //   $('.overlay').fadeToggle(200);
+  //   $('.menu-btn a').toggleClass('btn-open').toggleClass('btn-close');
+  // });
+  $('.menuu a').on('click', function () {
     $('.overlay').fadeToggle(200);
     $('.menu-btn a').toggleClass('btn-open').toggleClass('btn-close');
   });
+});
+
+$(document).ready(function () {
+  $('.btn-mobile-open').click(function () {
+    $('.overlay').fadeToggle(200);
+    $(this).toggleClass('btn-mobile-open').toggleClass('btn-close');
+  });
+  $('.overlay').on('click', function () {
+    $('.overlay').fadeToggle(200);
+    $('.menu-btn a').toggleClass('btn-mobile-open').toggleClass('btn-close');
+  });
   $('.menu a').on('click', function () {
     $('.overlay').fadeToggle(200);
-    $('.menu-btn a').toggleClass('btn-open').toggleClass('btn-close');
+    $('.menu-btn a').toggleClass('btn-mobile-open').toggleClass('btn-close');
   });
 });
